eb2ea4d224611f7b4465b4de846b66899298db0d
saidhaille3 <167263633+saidhaille3@users.noreply.github.com>
2026-08-08 16:33:39 -0500

    fjndrn bakcend iodtae cors

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,19 @@ const admin = require('firebase-admin');
 
 // --- INIT ---
 const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
+
 app.use(express.json());
 app.use(express.static('../'));
+
+
 
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
